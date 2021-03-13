8fbad6e7f971e04a284c5e718d2199a8ceecc654
Michel Ângelo <xsenick@debian.xsenick.org>
2021-03-13 14:34:26 -0300

    commit corrections and README update

---
+++ README.md
@@ -0,0 +1,18 @@
+## PRIMEIRO PROJETO COM JAVASCRIPT, CSS & HTML
+
+### O projeto baseia-se no propósito de mostrar a hora real e alterar cores de acordo com as condições a seguir:
+
+#### hora < 12 : Manhã
+
+<img src="readmephotos/dia.png" alt="dia" width="50%">
+<hr>
+
+#### hora >= 12hr e hora < 18hr : Tarde
+
+<img src="readmephotos/tarde.png" alt="tarde" width="50%">
+<hr>
+
+#### hora >= 18 : Noite
+
+<img src="readmephotos/noite.png" alt="noite" width="50%">
+<hr>
--- scripts/index.js
+++ scripts/index.js
@@ -1,29 +1,29 @@
 function loader() {
-    const msg = document.querySelector('div.msg');
-    const img = document.querySelector('.image');
-    const backSection = document.querySelector('section#content');
-    const title = document.getElementById('title');
+	const msg = document.querySelector("div.msg");
+	const img = document.querySelector(".image");
+	const backSection = document.querySelector("section#content");
+	const title = document.getElementById("title");
 
-    let data = new Date();
-    let hours = data.getHours();
-
-    if (hours >= 18) {
-        msg.innerText = `Agora são ${hours} horas da noite`;
-        img.src = 'src/noite.png';
-        title.style.color = '#e1d5a7e';
-        document.body.style.background = '#2d2d5c';
-        backSection.style.background = '#706d76';
-    } else if (hours >= 12 && hours < 18) {
-        msg.innerText = `Agora são ${hours} horas da tarde`;
-        img.src = 'src/tarde.png';
-        title.style.color = '#523320';
-        document.body.style.background = '#ffa64e';
-        backSection.style.background = '#ffda25';
-    } else {
-        msg.innerText = `Agora são ${hours} horas da manhã`;
-        img.src = 'src/dia.png';
-        title.style.color = '#ffffff';
-        document.body.style.background = '#007bff';
-        backSection.style.background = '#d0e2f0';
-    }
+	let data = new Date();
+	let hours = data.getHours();
+	
+	if (hours >= 18) {
+		msg.innerText = `Agora são ${hours} hora(s) da noite`;
+		img.src = "src/noite.png";
+		title.style.color = "#e1d5a7e";
+		document.body.style.background = "#2d2d5c";
+		backSection.style.background = "#706d76";
+	} else if (hours >= 12 && hours < 18) {
+		msg.innerText = `Agora são ${hours} hora(s) da tarde`;
+		img.src = "src/tarde.png";
+		title.style.color = "#523320";
+		document.body.style.background = "#ffa64e";
+		backSection.style.background = "#ffda25";
+	} else {
+		msg.innerText = `Agora são ${hours} hora(s) da manhã`;
+		img.src = "src/dia.png";
+		title.style.color = "#ffffff";
+		document.body.style.background = "#007bff";
+		backSection.style.background = "#d0e2f0";
+	}
 }
