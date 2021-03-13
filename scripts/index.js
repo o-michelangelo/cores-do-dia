c42f66415cfcc6cb5889e708949ca639e13251da
Michel Ângelo <xsenick@debian.xsenick.org>
2021-03-13 14:05:08 -0300

    backend site uploaded

---
+++ scripts/index.js
@@ -0,0 +1,29 @@
+function loader() {
+    const msg = document.querySelector('div.msg');
+    const img = document.querySelector('.image');
+    const backSection = document.querySelector('section#content');
+    const title = document.getElementById('title');
+
+    let data = new Date();
+    let hours = data.getHours();
+
+    if (hours >= 18) {
+        msg.innerText = `Agora são ${hours} horas da noite`;
+        img.src = 'src/noite.png';
+        title.style.color = '#e1d5a7e';
+        document.body.style.background = '#2d2d5c';
+        backSection.style.background = '#706d76';
+    } else if (hours >= 12 && hours < 18) {
+        msg.innerText = `Agora são ${hours} horas da tarde`;
+        img.src = 'src/tarde.png';
+        title.style.color = '#523320';
+        document.body.style.background = '#ffa64e';
+        backSection.style.background = '#ffda25';
+    } else {
+        msg.innerText = `Agora são ${hours} horas da manhã`;
+        img.src = 'src/dia.png';
+        title.style.color = '#ffffff';
+        document.body.style.background = '#007bff';
+        backSection.style.background = '#d0e2f0';
+    }
+}
