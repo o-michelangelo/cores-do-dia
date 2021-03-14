b2bcc61b24cbd0e97e4275ce3e6398dc00b33716
Michel Ângelo <xsenick@debian.xsenick.org>
2021-03-13 22:54:08 -0300

    width regulation

--- scripts/index.js
+++ scripts/index.js
@@ -6,22 +6,22 @@ function loader() {
 
 	let data = new Date();
 	let hours = data.getHours();
-	
+
 	if (hours >= 18) {
 		msg.innerText = `Agora são ${hours} hora(s) da noite`;
-		img.src = "src/noite.png";
+		img.src = "src/noite150.png";
 		title.style.color = "#e1d5a7e";
 		document.body.style.background = "#2d2d5c";
 		backSection.style.background = "#706d76";
 	} else if (hours >= 12 && hours < 18) {
 		msg.innerText = `Agora são ${hours} hora(s) da tarde`;
-		img.src = "src/tarde.png";
+		img.src = "src/tarde150.png";
 		title.style.color = "#523320";
 		document.body.style.background = "#ffa64e";
 		backSection.style.background = "#ffda25";
 	} else {
 		msg.innerText = `Agora são ${hours} hora(s) da manhã`;
-		img.src = "src/dia.png";
+		img.src = "src/dia150.png";
 		title.style.color = "#ffffff";
 		document.body.style.background = "#007bff";
 		backSection.style.background = "#d0e2f0";
